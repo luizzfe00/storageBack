91e2319bc641706bc54c1803c1f011ad3f509aa0
Luiz Felipe <luizzfelip00@gmail.com>
2021-04-11 20:26:39 -0300

    Signin

--- application/api/v1/auth.routes.js
+++ application/api/v1/auth.routes.js
@@ -4,6 +4,6 @@ const AuthController = require('../../middlewares/controllers/auth.handler');
 
 const router = express.Router();
 
-router.post('/', AuthController.handleProducerLogin);
+router.post('/signin', AuthController.handleProducerLogin);
 
 module.exports = router;
